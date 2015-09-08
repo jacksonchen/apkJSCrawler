b6eeeb8c424118d6ba7aa6244a8376f7052c4548
Jackson Chen <tiger2chen@gmail.com>
2015-09-08 17:51:09 -0600

    Add logging

--- loop.py
+++ loop.py
@@ -3,6 +3,5 @@ import subprocess
 
 with open(sys.argv[1]) as f:
   for line in f:
-    print "Analyzing keyword " + line.rstrip('\n')
     COMMAND = "node bin/apkJSCrawler.js keyword " + line.rstrip('\n') + " " + sys.argv[2] + " ../apkJSCrawler-GooglePlay"
     subprocess.call(COMMAND, shell=True)
